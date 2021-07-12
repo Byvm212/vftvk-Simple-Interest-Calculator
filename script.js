ec77da494cc6337fd135b4202fbcaa5272fe72f3
Brenda <brendablah061@gmail.com>
2021-07-12 14:11:38 -0400

    final project

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+//here the parameters through which the numbers will pass and, the conditions that must be met for the page to yield a valid result//
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    if (principal<=0){
+        document.getElementById("result").innerHTML="Please enter a positive number!"
+    }
+    else{
+    var interest = principal * years * rate/100
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
+    }}
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + "%";
 }
         
