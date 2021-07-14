b4b922b21ab76922c3e91d3efc6afa565cfad717
Brenda <brendablah061@gmail.com>
2021-07-14 13:27:55 -0400

    final project

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 //here the parameters through which the numbers will pass and, the conditions that must be met for the page to yield a valid result//
-function compute()
+function compute() 
 {
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    if (principal<=0){
-        document.getElementById("result").innerHTML="Please enter a positive number!"
-    }
-    else{
-    var interest = principal * years * rate/100
-    var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
-    }}
+    var years = document.getElementById("years").value; //Gets all the values from html (Amount, rate and number of years).
 
-function updateRate()
-{
+    if (principal<=0){ 
+        alert("Please enter a positive number!");
+        document.getElementById("principal").focus();
+    }
+
+    else{ 
+        var interest = principal * years * rate / 100;
+        var year = new Date().getFullYear()+parseInt(years);
+        document.getElementById("result").innerHTML = "If you deposit: <a style='background-color:yellow'>" + principal + "</a><br>" + `
+        at an interest rate of: <a style='background-color:yellow'>` + rate + "</a><br>" + `You will receive an amount of: 
+        <a style='background-color:yellow'>` + interest + "</a><br>" + "In the year: <a style='background-color:yellow'>" + year + "</a><br>";
+
+       
+
+       
+
+    }
+
+    
+    
+}
+
+function updateRate(){ 
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText = rateval + "%";
 }
         
+
